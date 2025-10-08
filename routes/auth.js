87cefc7c7e6530a4e3f45b65e7c97db02e57d497
Allen-pogi <143931506+Allen-pogi@users.noreply.github.com>
2025-10-08 16:53:39 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/authController");
-const protect = require("../middleware/auth");
+const authController = require("../controllers/authcontroller");
+const protect = require("../middleware/auth"); // middleware that verifies JWT
+
+/* ---------------------- USER ROUTES ---------------------- */
 
 // Public routes
 router.post("/user/register", authController.registerUser);
@@ -9,5 +11,14 @@ router.post("/user/login", authController.loginUser);
 
 // Protected route
 router.get("/user/me", protect, authController.getCurrentUser);
+
+/* ---------------------- ADMIN ROUTES ---------------------- */
+
+// Public routes
+router.post("/admin/register", authController.registerAdmin);
+router.post("/admin/login", authController.loginAdmin);
+
+// Protected route
+router.get("/admin/me", protect, authController.getCurrentAdmin);
 
 module.exports = router;
