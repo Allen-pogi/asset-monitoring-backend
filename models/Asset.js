69e731b4d7eac866c7881f4911deee08a48b7e69
Allen-pogi <143931506+Allen-pogi@users.noreply.github.com>
2025-10-09 08:18:23 +0800

    Create Asset.js

---
+++ models/Asset.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const assetSchema = new mongoose.Schema(
+  {
+    assetName: { type: String, required: true },
+    classification: { type: String },
+    description: { type: String },
+    category: { type: String },
+    serialNumber: { type: String },
+    purchaseDate: { type: Date },
+    issuedDate: { type: Date },
+    issuedTo: { type: String },
+    generateQR: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Asset", assetSchema);
